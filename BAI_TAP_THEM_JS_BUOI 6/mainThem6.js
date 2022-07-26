fbd77e1f4ec1f728d67fe95e7315a20b1d29067e
LeThu <thuntl.vnit@gmail.com>
2022-07-26 19:53:50 +0700

    push bài tập làm thêm JS buổi 6

--- BAI_TAP_THEM_JS_BUOI 6/mainThem6.js
+++ BAI_TAP_THEM_JS_BUOI 6/mainThem6.js
@@ -1,29 +1,13 @@
-function inSoNguyenTo() {
-  for (
-    var e = document.getElementById("inputNn").value, t = "", n = 1;
-    n <= e;
-    n++
-  ) {
-    checkPrime(n) && (t += " " + n);
+document.getElementById("inSoNguyenTo").onclick = function () {
+  var e = document.getElementById("inputNn").value,
+    t = "";
+  for (n = 1; n <= e; n++) {
+    if (checkPrime(n)) t += " " + n;
   }
   document.getElementById("showSoNguyenTo").innerHTML = t;
-}
+};
 function checkPrime(e) {
-  if (e < 2) return !1;
-  for (var t = 2; t <= Math.sqrt(e); t++) if (e % t == 0) return !1;
-  return !0;
+  if (e < 2) return false;
+  for (var t = 2; t <= Math.sqrt(e); t++) if (e % t == 0) return false;
+  return true;
 }
-document.addEventListener(
-  "contextmenu",
-  function (e) {
-    e.preventDefault();
-  },
-  !1
-),
-  (document.onkeydown = function (e) {
-    return (
-      123 != (e = e || window.event).keyCode &&
-      (!e.ctrlKey || !e.shiftKey || 73 != e.keyCode) &&
-      void 0
-    );
-  });
